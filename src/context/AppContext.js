9e49979ec43a88578ae7217391c05f2777104e12
Irascible Ardent <irascible_ardent@pm.me>
2024-05-28 17:43:39 -0400

    Final Project Complete; final upload 	modified:   .gitignore 	modified:   src/App.js 	modified:   src/components/AllocationForm.js 	modified:   src/components/Budget.js 	new file:   src/components/CurrencyDropdown.js 	modified:   src/components/ExpenseItem.js 	modified:   src/components/ExpenseList.js 	modified:   src/components/ExpenseTotal.js 	modified:   src/components/Remaining.js 	modified:   src/context/AppContext.js

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -30,21 +30,20 @@ export const AppReducer = (state, action) => {
                     ...state
                 }
             }
-
-        case 'RED_EXPENSE':
-            const red_expenses = state.expenses.map((currentExp)=> {
-                if (currentExp.name === action.payload.name && currentExp.cost - action.payload.cost >= 0) {
-                    currentExp.cost =  currentExp.cost - action.payload.cost;
-                    budget = state.budget + action.payload.cost
-                }
-                return currentExp
-            })
-            action.type = "DONE";
-            return {
-                ...state,
-                expenses: [...red_expenses],
-            };
-        case 'DELETE_EXPENSE':
+            case 'RED_EXPENSE':
+                const red_expenses = state.expenses.map((currentExp)=> {
+                    if (currentExp.name === action.payload.name && currentExp.cost - action.payload.cost >= 0) {
+                        currentExp.cost =  currentExp.cost - action.payload.cost;
+                        budget = state.budget + action.payload.cost
+                    }
+                    return currentExp
+                })
+                action.type = "DONE";
+                return {
+                    ...state,
+                    expenses: [...red_expenses],
+                };
+            case 'DELETE_EXPENSE':
             action.type = "DONE";
             state.expenses.map((currentExp)=> {
                 if (currentExp.name === action.payload) {
@@ -61,6 +60,7 @@ export const AppReducer = (state, action) => {
         case 'SET_BUDGET':
             action.type = "DONE";
             state.budget = action.payload;
+
             return {
                 ...state,
             };
@@ -70,6 +70,7 @@ export const AppReducer = (state, action) => {
             return {
                 ...state
             }
+
         default:
             return state;
     }
